9afac5ead97ec86e3ec72309870820355717e427
chris.scheid <chris.scheid@735f95c2-4d4f-0410-895f-4b053a011349>
2008-06-14 06:30:05 +0000

    - implemented functionality to let the template wizard create the template and did some cleanup - wizard now fulfills the requirement (issue-1) and can be considered as deployable

--- loom-core/src/org/digitalsoul/loom/core/CreateTemplateAction.java
+++ loom-core/src/org/digitalsoul/loom/core/CreateTemplateAction.java
@@ -2,7 +2,6 @@ package org.digitalsoul.loom.core;
 
 import org.digitalsoul.loom.core.prefs.Preferences;
 import org.digitalsoul.loom.core.wizard.CreateTemplateWizard;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -14,22 +13,18 @@ import org.eclipse.ui.IWorkbenchPart;
 
 public class CreateTemplateAction implements IObjectActionDelegate {
 
-    private IResource selectedFile;
     private IJavaElement javaElement;
 
     public void run(IAction action) {
         String templateFilename = javaElement.getElementName();
         templateFilename = templateFilename.replace(LoomConstants.JAVA_FILE_EXTENSION, Preferences.getTemplateFileExtension());
-        String relativeFolder = selectedFile.getParent().getProjectRelativePath().toPortableString();
-        String projectPath = selectedFile.getProject().getLocation().toPortableString();
-        CreateTemplateWizard wizard = new CreateTemplateWizard(javaElement, templateFilename, projectPath, relativeFolder);
+        CreateTemplateWizard wizard = new CreateTemplateWizard(javaElement, templateFilename);
         new WizardDialog(LoomCorePlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell(), wizard).open(); 
     }
 
     public void selectionChanged(IAction action, ISelection _selection) {
         IStructuredSelection selection = (IStructuredSelection) _selection;
         this.javaElement = (IJavaElement)selection.getFirstElement();
-        this.selectedFile = javaElement.getResource();
     }
 
     public void setActivePart(IAction action, IWorkbenchPart targetPart) {
