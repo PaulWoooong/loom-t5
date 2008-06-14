bb16d79c6607bfccf25f41a0bd916f967d228c0e
chris.scheid <chris.scheid@735f95c2-4d4f-0410-895f-4b053a011349>
2008-06-14 04:15:22 +0000

    minor cleanup

--- trunk/loom-core/src/org/digitalsoul/loom/core/wizard/CreateTemplateWizard.java
+++ trunk/loom-core/src/org/digitalsoul/loom/core/wizard/CreateTemplateWizard.java
@@ -7,24 +7,16 @@ import org.eclipse.jface.wizard.Wizard;
 public class CreateTemplateWizard extends Wizard {
 
     private CreateTemplateWizardPage page;
-//    private String relativeFolder;
- //   private String projectPath;
-   // private String filename;
     private IJavaElement javaElement;
 
     public CreateTemplateWizard(IJavaElement javaElement, String filename, String projectPath, String relativeFolder) {
-   //     this.filename = filename;
         this.javaElement = javaElement;
         //filename = filename.replace("." + LoomConstants.JAVA_FILE_EXTENSION, "." + LoomConstants.TEMPLATE_FILE_EXTENSION);
-       // this.projectPath = projectPath;
-        //this.relativeFolder = relativeFolder;
-        //System.err.println(projectPath);
         page = new CreateTemplateWizardPage("Create Template");
         page.setDescription("Choose a package and a name for your Template");
         page.init(javaElement, filename);
         //setWindowTitle("Create Template");
         addPage(page);
-        
     }
     
     @Override
