755430875d2c5361b2fc907718f2356ea86c5c82
chris.scheid <chris.scheid@735f95c2-4d4f-0410-895f-4b053a011349>
2008-08-09 22:51:36 +0000

    deleted unused wizard classes and renamed template rename action

--- trunk/loom-core/src/org/digitalsoul/loom/core/CreateTemplateAction.java
+++ trunk/loom-core/src/org/digitalsoul/loom/core/CreateTemplateAction.java
@@ -1,27 +1,18 @@
 package org.digitalsoul.loom.core;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-
-import org.digitalsoul.loom.core.prefs.Preferences;
-import org.digitalsoul.loom.core.wizard.CreateTemplateWizard;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 
-public class StartTemplateWizardAction implements IObjectActionDelegate {
+public class CreateTemplateAction implements IObjectActionDelegate {
 
     private ICompilationUnit javaElement;
 
     public void run(IAction action) {
-//        String templateFilename = javaElement.getElementName();
-//        templateFilename = templateFilename.replace(LoomConstants.JAVA_FILE_EXTENSION, Preferences.getTemplateFileExtension());
-//        CreateTemplateWizard wizard = new CreateTemplateWizard(javaElement, templateFilename);
-//        new WizardDialog(LoomCorePlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getShell(), wizard).open();
         new TemplateBuilder().createTemplate(javaElement);
     }
 
