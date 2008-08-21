edda962162f3baef47f064765d24531159f7dd64
chris.scheid <chris.scheid@735f95c2-4d4f-0410-895f-4b053a011349>
2008-08-21 20:57:02 +0000

    improvement: added fix for hardcoded javaFilePath replacement

--- trunk/loom-core/test/org/digitalsoul/loom/core/ProjectBasedTest.java
+++ trunk/loom-core/test/org/digitalsoul/loom/core/ProjectBasedTest.java
@@ -25,18 +25,20 @@ public class ProjectBasedTest extends BaseTester {
     protected IFolder templateFolder;
     protected IFile javaFile;
     protected IJavaProject javaProject;
-    protected ICompilationUnit compilationUnit;
+    protected ICompilationUnit javaCompilationUnit;
     protected ProjectBuilder builder;
+    private ICompilationUnit kawaCompilationUnit;
+    private IFile kawaFile;
 
     /**
      * 
      */
     @Test
     public void testJavaFileCorrectEnvironment() {
-        Assert.assertEquals("org.loom", compilationUnit.getParent().getElementName());
-        Assert.assertEquals("java", compilationUnit.getParent().getParent().getElementName());
-        Assert.assertEquals(IJavaElement.PACKAGE_FRAGMENT, compilationUnit.getParent().getElementType());
-        Assert.assertEquals(IJavaElement.PACKAGE_FRAGMENT_ROOT, compilationUnit.getParent().getParent().getElementType());
+        Assert.assertEquals("org.loom", javaCompilationUnit.getParent().getElementName());
+        Assert.assertEquals("javaxx", javaCompilationUnit.getParent().getParent().getElementName());
+        Assert.assertEquals(IJavaElement.PACKAGE_FRAGMENT, javaCompilationUnit.getParent().getElementType());
+        Assert.assertEquals(IJavaElement.PACKAGE_FRAGMENT_ROOT, javaCompilationUnit.getParent().getParent().getElementType());
     }
     
     /**
@@ -49,19 +51,19 @@ public class ProjectBasedTest extends BaseTester {
             javaProject = builder.javaProject;
             IFolder srcFolder = builder.createFolder("src");
             IFolder mainFolder = builder.createFolder(srcFolder, "main");
-            javaFolder = builder.createFolder(mainFolder, "java");
-
-            IClasspathEntry sourceEntry = JavaCore.newSourceEntry(javaFolder.getFullPath());
-            javaProject.setRawClasspath(new IClasspathEntry[] {sourceEntry}, null);
+            javaFolder = builder.createFolder(mainFolder, "javaxx");
+            
+            IClasspathEntry javaSourceEntry = JavaCore.newSourceEntry(javaFolder.getFullPath());
+            
+            javaProject.setRawClasspath(new IClasspathEntry[] {javaSourceEntry}, null);
 
             templateFolder = builder.createFolder(mainFolder, "webapp");
             Assert.assertEquals(1, javaProject.getAllPackageFragmentRoots().length);
 
-            IPackageFragmentRoot javaRoot = javaProject.getAllPackageFragmentRoots()[0];
-            IPackageFragment orgFragment = javaRoot.createPackageFragment("org.loom", true, null);
-            compilationUnit = orgFragment.createCompilationUnit("Wizard.java", "public class Wizard{}", true, null);
+            javaCompilationUnit = buildJavaFile(0); 
             
-            javaFile = (IFile) compilationUnit.getResource();
+            javaFile = (IFile) javaCompilationUnit.getResource();
+            
             Assert.assertTrue(javaFile.exists());
             Assert.assertTrue(javaFolder.exists());
             Assert.assertTrue(templateFolder.exists());
@@ -69,6 +71,23 @@ public class ProjectBasedTest extends BaseTester {
         catch (CoreException e) {
             e.printStackTrace();
         }    
+    }
+
+    /**
+     * @param i
+     * @return
+     */
+    private ICompilationUnit buildJavaFile(int i) {
+        ICompilationUnit unit = null;
+        try {
+            IPackageFragmentRoot javaRoot = javaProject.getAllPackageFragmentRoots()[i];
+            IPackageFragment orgFragment = javaRoot.createPackageFragment("org.loom", true, null);
+            unit = orgFragment.createCompilationUnit("Wizard.java", "public class Wizard{}", true, null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return unit;
     }
 
     /**
